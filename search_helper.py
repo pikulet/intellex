73c836cbc2c9a0986b8585f8583a331962c4e466
Joyce <jycyeo@yahoo.com.sg>
2019-03-29 08:51:25 +0800

    search.py: Add file reading and query parsing

---
+++ search_helper.py
@@ -0,0 +1,51 @@
+import pickle
+
+########################### DEFINE CONSTANTS ###########################
+CONJUNCTION_OPERATOR = " AND "
+PHRASE_MARKER = "\""
+######################## FILE READING FUNCTIONS ########################
+
+### Retrieve a dictionary mapping docIDs to normalised document lengths
+###
+def get_lengths(p):
+    p.seek(0)
+    length_dict = pickle.load(p)
+    return length_dict
+
+### Retrieve a dictionary format given the dictionary file
+###
+def get_dictionary(dictionary_file):
+    with open(dictionary_file, 'rb') as f:
+        dictionary = pickle.load(f)
+    return dictionary
+
+### Retrieve a query format given the query file
+def get_query(query_file):
+    with open(query_file, 'r') as f:
+        data = f.read().splitlines()
+
+    query_text = parse_query(data[0])
+    positive_list = [int(x) for x in data[1:] ]
+    return query_text, positive_list
+
+######################## QUERY PROCESSING ########################
+
+def parse_query(q):
+    q = q.split(CONJUNCTION_OPERATOR)
+    is_phrase = lambda s: s.startswith(PHRASE_MARKER)
+    parse_phrase = lambda s: s[1: -1].split()
+
+    result = list()
+    for t in q:
+        if is_phrase(t):
+            result.append(parse_phrase(t))
+        else:
+            result.append(t)
+
+    print(result)
+    return result
+def process_query(p, dictionary, q):
+    lengths = get_lengths(p)
+    return [1, 2, 3]
+
+
