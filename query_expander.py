69191c45fb5910238924b6166209a1d9c0dc1bc7
adjscent <jasonsohjsh@gmail.com>
2019-04-05 23:37:58 +0800

    patch query expander

--- query_expander.py
+++ query_expander.py
@@ -2,6 +2,10 @@ from search_helper import *
 from data_helper import *
 from pprint import pprint
 import heapq
+try:
+    from tqdm import tqdm
+except ImportError:
+    tqdm = lambda *i, **kwargs: i[0]
 
 ########################### DEFINE CONSTANTS ###########################
 
@@ -28,6 +32,12 @@ def get_vector_from_docID(docID):
     # vector are stored as sparse indexes
     # each valid index will map to (tf, idf)
     offset = vector_dict[docID]
+    data = load_data_with_handler(vector_value, offset)
+    return data
+
+def get_vector_from_offset(offset):
+    # vector are stored as sparse indexes
+    # each valid index will map to (tf, idf)
     data = load_data_with_handler(vector_value, offset)
     return data
 
@@ -61,10 +71,10 @@ def get_new_query_vector(original_vector, docIDs):
     return vector
 
 def query_parallel_util(pair):
-    docID, vector = pair
+    docID, offset, vector = pair
     score = 0.
+    docVector = get_vector_from_offset(offset)
     for key, value in vector.items():  
-            docVector = get_vector_from_docID(docID)
             if key in docVector:
                 score += extractValue(docVector[key]) * value
     return docID, score
@@ -72,10 +82,12 @@ def query_parallel_util(pair):
 import multiprocessing
 
 def query_parallel(vector, N):
+    vector_dict = load_data("dictionaryvector.txt")
+
     score_list = []
     with multiprocessing.Pool(4) as pool:
-        result = pool.imap_unordered(query_parallel_util, [(docID, vector) for docID in vector_dict.keys()], chunksize=1)
-        for docID, score in result:
+        result = pool.imap_unordered(query_parallel_util, [(docID, offset, vector) for docID, offset in vector_dict.items()], chunksize=50)
+        for docID, score in tqdm(result, total=len(vector_dict.keys())):
             score_list.append((-score, docID))
     top_results = heapq.nsmallest(N, score_list, key=lambda x: (x[0], x[1]))  # smallest since min_heap is used
     top_documents = list(map(lambda x: str(x[1]), top_results))
@@ -84,10 +96,10 @@ def query_parallel(vector, N):
 
 def query(vector, N):
     score_list = []
-    for docID in vector_dict.keys():
+    for docID in tqdm(vector_dict.keys(), total = len(vector_dict.keys())):
         score = 0.
+        docVector = get_vector_from_docID(docID)
         for key, value in vector.items():  
-            docVector = get_vector_from_docID(docID)
             if key in docVector:
                 score += extractValue(docVector[key]) * value
         score_list.append((-score, docID))
@@ -103,6 +115,7 @@ def coin_toss(value):
     return value if random.random() < 0.5 else 0
 
 if __name__ == '__main__':
+    vector_dict = load_data("dictionaryvector.txt")
     import time
     start = time.time()
     # get_vector(246788)
