1b12990313e5d76e21a08c7aadaccf3439e3c470
adjscent <jasonsohjsh@gmail.com>
2019-04-18 01:04:56 +0800

    some helper python scripts

---
+++ lookvector.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import sys
+from pprint import pprint
+from data_helper import *
+from QueryExpansion import *
+
+file_no = ""
+if len(sys.argv) > 1 :
+    file_no = sys.argv[1].strip()
+else:
+    print("What document id do you want to see")
+    file_no = input().strip()
+
+d = get_new_query_offset([int(file_no)])
+from operator import itemgetter
+for k, v in sorted(d.items(), key=itemgetter(1)):
+    print (k, v)
